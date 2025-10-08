2e7b0a03b561d8dd40fec684ca22d9ea203c712b
ivtune <111287502+ivtune@users.noreply.github.com>
2025-10-08 10:22:22 +0100

    Day 2: Mad-Libs with f-strings

---
+++ mad_libs.py
@@ -0,0 +1,10 @@
+#<name><place><adjective><noun><verb>
+#Yesterday, Sam walked into a library and saw a purple penguin dancing!
+
+name= input("Enter a NAME: ")
+place=input("Enter a PLACE: ")
+adjective= input("Enter an ADJECTIVE: ")
+noun = input("Enter a NOUN: ")
+verb=input("ENter a VERB: ")
+
+print (f"Yesterday, {name} walked into a {place} and saw a {adjective} {noun} {verb}!\n Story complete.😊")
